18256a1da15beba10289ad90542de244ce20eb86
Haridev P <haridevpnarayananivas@gmail.com>
2025-11-26 22:00:53 +0530

    Updated Trophy Room: Added Credly links and removed outdated certs

--- src/App.jsx
+++ src/App.jsx
@@ -84,10 +84,8 @@ const BLOG_POSTS = [
 ];
 
 const TROPHIES = [
-  { title: "Google Cybersecurity Cert", issuer: "Google", date: "2023", icon: "G" },
-  { title: "Top 1% Rank", issuer: "TryHackMe", date: "2024", icon: "THM" },
-  { title: "Dante Pro Lab", issuer: "HackTheBox", date: "2023", icon: "HTB" },
-  { title: "Certified Ethical Hacker", issuer: "EC-Council", date: "2022", icon: "CEH" },
+  { title: "Google Cybersecurity Cert", issuer: "Google", date: "2023", icon: "G", link: "https://www.credly.com/badges/1aadb2c4-5166-4838-b412-8a212812a645/" },
+  { title: "Google Cloud Computing Foundations", issuer: "Google Cloud", date: "2023", icon: "GCP", link: "https://www.credly.com/badges/e9868959-964f-4194-b640-77d4b54ac790/" },
   { title: "GDSC Member '23-24", issuer: "Google Developers", date: "2024", icon: "GDSC", link: "https://developers.google.com/profile/badges/community/gdsc/2023/member" },
   { title: "Cloud Skills Boost", issuer: "Google Cloud", date: "Active", icon: "GCP", link: "https://www.cloudskillsboost.google/public_profiles/85843ac3-3173-4aa4-85bc-5e02126aa3cb" }
 ];
